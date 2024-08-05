f311ae48b41b905767d3ec8690cab9d49c688776
Prem Eide <premeide@gmail.com>
2024-08-05 13:30:13 +0200

    - fix syntax

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NFCPassportReader",
-            path: "NFCPassportReader",
-            dependencies: ["OpenSSL"]),
+            dependencies: ["OpenSSL"],
+            path: "NFCPassportReader"),
     ]
 )
 
